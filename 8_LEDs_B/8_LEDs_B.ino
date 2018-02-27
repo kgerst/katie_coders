89502abcd90984fd407da476f3a22fab083e3c38
Kim Gerst <kimgerst@gmail.com>
2018-02-27 15:42:22 -0600

    fix line endings

--- 8_LEDs_B/8_LEDs_B.ino
+++ 8_LEDs_B/8_LEDs_B.ino
@@ -1,1 +1,47 @@
-const int DELAY_TIME = 500;void setup() {  pinMode(2, OUTPUT);  pinMode(3, OUTPUT);  pinMode(4, OUTPUT);  pinMode(5, OUTPUT);  pinMode(6, OUTPUT);  pinMode(7, OUTPUT);  pinMode(8, OUTPUT);  pinMode(9, OUTPUT);}void loop() {  digitalWrite(2, HIGH);  delay(DELAY_TIME);  digitalWrite(3, HIGH);  delay(DELAY_TIME);  digitalWrite(4, HIGH);  delay(DELAY_TIME);  digitalWrite(5, HIGH);  delay(DELAY_TIME);  digitalWrite(6, HIGH);  delay(DELAY_TIME);  digitalWrite(7, HIGH);  delay(DELAY_TIME);  digitalWrite(8, HIGH);  delay(DELAY_TIME);  digitalWrite(9, HIGH);  delay(DELAY_TIME);  digitalWrite(9, LOW);  delay(DELAY_TIME);  digitalWrite(8, LOW);  delay(DELAY_TIME);  digitalWrite(7, LOW);  delay(DELAY_TIME);  digitalWrite(6, LOW);  delay(DELAY_TIME);  digitalWrite(5, LOW);  delay(DELAY_TIME);  digitalWrite(4, LOW);  delay(DELAY_TIME);  digitalWrite(3, LOW);  delay(DELAY_TIME);  digitalWrite(2, LOW);  delay(DELAY_TIME);}
+const int DELAY_TIME = 500;
+
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(2, HIGH);
+  delay(DELAY_TIME);
+  digitalWrite(3, HIGH);
+  delay(DELAY_TIME);
+  digitalWrite(4, HIGH);
+  delay(DELAY_TIME);
+  digitalWrite(5, HIGH);
+  delay(DELAY_TIME);
+  digitalWrite(6, HIGH);
+  delay(DELAY_TIME);
+  digitalWrite(7, HIGH);
+  delay(DELAY_TIME);
+  digitalWrite(8, HIGH);
+  delay(DELAY_TIME);
+  digitalWrite(9, HIGH);
+  delay(DELAY_TIME);
+  digitalWrite(9, LOW);
+  delay(DELAY_TIME);
+  digitalWrite(8, LOW);
+  delay(DELAY_TIME);
+  digitalWrite(7, LOW);
+  delay(DELAY_TIME);
+  digitalWrite(6, LOW);
+  delay(DELAY_TIME);
+  digitalWrite(5, LOW);
+  delay(DELAY_TIME);
+  digitalWrite(4, LOW);
+  delay(DELAY_TIME);
+  digitalWrite(3, LOW);
+  delay(DELAY_TIME);
+  digitalWrite(2, LOW);
+  delay(DELAY_TIME);
+}
